26df63e8a22179c86556c36a5ab4b06a72ffec7c
dushyanth1409 <150711080+dushyanth1409@users.noreply.github.com>
2023-11-13 20:14:37 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies your project may have
+        'random',
+        'unittest',
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz:math_quiz',
+        ],
+    },
+    author='dushyanth yadanaparthi',
+    author_email='dushyanth.yadanaparthi@fau.de',
+    description='A simple math quiz game',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: Apache License',
+        'Operating System :: OS Independent',
+    ],
+)
