0d31f79996411e9bf4ed5925864b4a6b8bc48793
dushyanth1409 <150711080+dushyanth1409@users.noreply.github.com>
2023-11-13 20:23:47 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         # Add any dependencies your project may have
-        'unittest',
+        
     ],
     entry_points={
         'console_scripts': [
